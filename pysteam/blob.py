8be6bc33edbda1428bbbf7be8ed86a51b13d26d1
Jasper St. Pierre <jstpierre@mecheye.net>
2012-04-30 06:16:09 -0400

    blob: Simplify reading a ton

--- pysteam/blob.py
+++ pysteam/blob.py
@@ -15,18 +15,18 @@ class Blob(object):
         self.ordered_children = []
         self.padding = 0
 
-    def parse(self, stream, recurse=True):
+    def parse(self, stream):
         mode, = struct.unpack("<H", stream.read(2))
         if mode == Blob.MAGIC:
             length = struct.unpack("<l", stream.read(4))[0] - 10
             self.padding, = struct.unpack("<L", stream.read(4))
 
-            while length >= 6:
+            end = stream.tell() + length
+            while stream.tell() < end:
                 node = BlobNode()
-                node.parse(stream, recurse)
+                node.parse(stream)
                 self.children[node.key] = node
                 self.ordered_children.append(node)
-                length -= 6 + node.desc_size + node.data_size
 
             stream.seek(self.padding, os.SEEK_CUR)
 
@@ -38,7 +38,7 @@ class Blob(object):
             compressed_bytes = stream.read(compressed_len)
             decompressed_bytes = zlib.decompress(compressed_bytes)
 
-            self.parse(StringIO(decompressed_bytes), recurse)
+            self.parse(StringIO(decompressed_bytes))
 
     def serialize(self, compress=True):
         mode = Blob.COMPRESSED_MAGIC if compress else Blob.MAGIC
@@ -65,39 +65,29 @@ class Blob(object):
 
 class BlobNode(object):
     def __init__(self):
-        self.child = None
         self.key = None
         self.data = None
-        self.data_size = 0
-        self.desc_size = 0
+        self.child = None
 
-    def parse(self, stream, recurse=True):
+    def parse(self, stream):
         # These are defined internally.
-        self.desc_size, self.data_size = struct.unpack("<HL", stream.read(6))
+        key_size, data_size = struct.unpack("<HL", stream.read(6))
+        self.key = stream.read(key_size)
+        self.data = stream.read(data_size)
+        if data_size < 2:
+            return
 
-        self.key = stream.read(self.desc_size)
-
-        # Get temp bytes (it appears that Valve is now trying
-        # to break parsers by putting null bytes after the blob node's
-        # data section) This method will use more memory and is slower
-        # but allows the parser to run with more sanity.
-        tempbytes = stream.read(self.data_size)
-
-        if recurse and len(tempbytes) >= 10:
-            tempshort, = struct.unpack("<H", tempbytes[:2])
-            if tempshort == Blob.MAGIC or (tempshort == Blob.COMPRESSED_MAGIC and len(tempbytes) >= 20):
-                self.child = Blob()
-                self.child.parse(StringIO(tempbytes))
-                return
-
-        self.data = tempbytes
+        magic, = struct.unpack("<H", self.data[:2])
+        if magic in (Blob.MAGIC, Blob.COMPRESSED_MAGIC):
+            self.child = Blob()
+            self.child.parse(StringIO(self.data))
 
     def serialize(self):
         if self.child is not None:
             data = self.child.serialize(False)
         else:
             data = self.data
-        return struct.pack("<HL", self.desc_size, self.data_size) + self.key + data
+        return struct.pack("<HL", len(self.key), len(data)) + self.key + data
 
     def __len__(self):
         if self.child is not None:
